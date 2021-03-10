d99faad7446838fd5493abbafd5d532c66cfcadc
Harshit Garg <harshitgargmnit@gmail.com>
2021-03-10 10:34:47 +0530

    Update | Examples

--- examples/registeration.sh
+++ examples/registeration.sh
@@ -9,14 +9,3 @@ curl --location --request POST 'localhost:9000/user_management/signup' \
     "email": "test@gmail.com",
     "password": "456789834"
 }'
-
-
-curl --location --request POST 'localhost:9000/user_management/signup' \
---header 'Content-Type: application/json' \
---header 'Cookie: csrftoken=PNdu30nbwJyRBUvPRTdZb69HdBwDNimMU3HAyjvR2wtuHJFqHaLbNXX3M3eBrYyW' \
---data-raw '{
-    "username": "hello",
-    "first_name": "Test",
-    "last_name": "R",
-    "password": "456789834"
-}'
